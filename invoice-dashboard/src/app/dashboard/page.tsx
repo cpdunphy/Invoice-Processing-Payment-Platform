aea9e6101f8bde30cd347262c75ce5d1b9cd2ef0
Wesley Sum <wesleysum0402@vt.edu>
2025-04-02 17:40:52 -0400

    Fixed global css error + home Page UI

--- invoice-dashboard/src/app/dashboard/page.tsx
+++ invoice-dashboard/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { Chart1 } from "@/components/chart1"
+import { AppSidebar } from "@/components/app-sidebar";
+import { Chart1 } from "@/components/chart1";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,21 +7,22 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import { ModeToggle } from "@/components/ModeToggle"
+} from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/ModeToggle";
+import { PieChart1 } from "@/components/pie-chart1";
 
 export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center justify-between px-4">
+        <header className="flex h-16 shrink-0 items-center justify-between px-4">
           {/* Left Section: Sidebar & Breadcrumb */}
           <div className="flex items-center gap-2">
             <SidebarTrigger className="-ml-1" />
@@ -38,14 +39,26 @@ export default function Page() {
           <ModeToggle />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <div className="rounded-xl bg-muted/50 h-32 p-4">
+              Total Invoices
+            </div>
+            <div className="rounded-xl bg-muted/50 h-32 p-4">Total Spend</div>
+            <div className="rounded-xl bg-muted/50 h-32 p-4">
+              Invoices Needing Review
+            </div>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <div className="rounded-xl bg-muted/50 md:col-span-2 p-4">
+              <Chart1 />
+            </div>
+            <div className="rounded-xl bg-muted/50 p-4">
+              <PieChart1 />
+            </div>
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
--- invoice-dashboard/src/app/invoices/upload/page.tsx
+++ invoice-dashboard/src/app/invoices/upload/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-
-
 import { useState } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import {
